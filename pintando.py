20186f8ff32fc6d261ac2ebd3705a32aee49add8
Hernan Salinas <hrn.salinas@outlook.com>
2021-09-13 21:25:32 -0500

    Agregar Rectangulo

--- pintando.py
+++ pintando.py
@@ -46,7 +46,17 @@ def circle(start, end):
 
 def rectangle(start, end):
     "Draw rectangle from start to end."
-    pass  # TODO
+    up()
+    goto(start.x, start.y)
+    down()
+    begin_fill()
+
+    for count in range(2):
+        forward(end.x - start.x)
+        left(90)
+        forward(end.x/2 - start.x/2)
+        left(90)
+    end_fill()
 
 
 def triangle(start, end):
